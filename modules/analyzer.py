c35c93f9a581a9eb382651438ff2fd1fe50334de
Nicola Ferralis <feranick@hotmail.com>
2017-09-25 20:36:51 -0400

    The Agilent 4155c is now full operational
    It includes empty methods to allow for an easy transition from sourcemeter (Keithley) to Agilent.
    No changes in ResultsWindow or Acquisition necessary.

--- modules/analyzer.py
+++ modules/analyzer.py
@@ -1,10 +1,10 @@
 '''
-sourcemeter.py
+analyzer.py
 -------------
 Class for providing a hardware support for 
-for the spectrum analyzer
+for the Agilent 4155C
 
-Version: 20170924
+Version: 20170925
 
 Copyright (C) 2017 Nicola Ferralis <ferralis@mit.edu>
 
@@ -14,49 +14,37 @@ the Free Software Foundation; either version 2 of the License, or
 (at your option) any later version.
 
 '''
-import visa
+
+import visa, time
+import numpy as np
 
 class Analyzer(object):
     '''
-    Analyzer Class
-        - Agilent 4155C
-        - command manual: https://wiki.epfl.ch/carplat/documents/4156_SCPI_Command_Reference.pdf
+    SourceMeter Class
+        - Agilent 4155C Semiconductor Parameter Analyzer
     '''
     def __init__(self, visa_string):
         self.manager = visa.ResourceManager().open_resource(visa_string)
         #self.manager = visa.ResourceManager('@py').open_resource(visa_string)
-
         print(self.ask("*IDN?"))
-        self.ask.("*RST")
-        self.write(":PAGE:CHAN:MODE SAMP")
+        self.write("*RST")
+        
+        self.write(":PAGE:CHAN:MODE SWEEP")
         self.write(":PAGE:CHAN:SMU3:DIS")
         self.write(":PAGE:CHAN:SMU4:DIS")
         self.write(":PAGE:CHAN:VSU1:DIS")
         self.write(":PAGE:CHAN:VSU2:DIS")
         self.write(":PAGE:CHAN:VMU1:DIS")
         self.write(":PAGE:CHAN:VMU2:DIS")
-        self.write(":PAGE:CHAN:SMU1:VNAME ‘VD’")
-        self.write(":PAGE:CHAN:SMU2:VNAME ‘VS’")
-        self.write(":PAGE:CHAN:SMU1:INAME ‘ID’")
-        self.write(":PAGE:CHAN:SMU2:INAME ‘IS’")
+        
+        self.write(":PAGE:CHAN:SMU1:VNAME 'VD'")
+        self.write(":PAGE:CHAN:SMU2:VNAME 'VS'")
+        self.write(":PAGE:CHAN:SMU1:INAME 'ID'")
+        self.write(":PAGE:CHAN:SMU2:INAME 'IS'")
+        self.write(":PAGE:CHAN:SMU1:MODE V")
         self.write(":PAGE:CHAN:SMU1:FUNC VAR1")
         self.write(":PAGE:CHAN:SMU2:MODE COMM")
         self.write(":PAGE:CHAN:SMU2:FUNC CONS")
-        
-        
-        # for safety
-        self.off()
-        self.write(":PAGE:MEAS:VAR1:START 0)
-        #self.write('SOUR:CURR 0')
-        #self.write('SOUR:VOLT 0')
-        
-        self.voltage_limit = 100.
-        self.current_limit = 1.
-
-        # init mode
-        self.set_mode('VOLT')
-        self.write('SYSTEM:BEEP:STATE OFF')
-        self.write('FORM:ELEM VOLT,CURR')
 
     def __del__(self):
         try:
@@ -76,96 +64,50 @@ class Analyzer(object):
     def ask(self, command):
         return self.manager.query(command)
 
-    ## keithley api
-    def get_mode(self, key):
-        if key.upper() == 'SOURCE':
-            return self.mode
-        else:
-            return 'CURR' if self.mode == 'VOLT' else 'VOLT'
-
-    def set_mode(self, mode):
-        """
-        Set source mode, CURRent or VOLTage. 
-        """
-        abv_mode = mode[0:4].upper()
-        if abv_mode in ['VOLT', 'CURR']:
-            self.mode = abv_mode
-        else:
-            raise ValueError('Wrong mode!')
-
-        self.write('SOUR:FUNC {}'.format(self.get_mode('source')))
-        self.write('SOUR:{}:MODE FIX'.format(self.get_mode('source')))
-        self.write('SOUR:{}:RANG:AUTO ON'.format(self.get_mode('source')))
-
-        self.write('SENS:FUNC "{}"'.format(self.get_mode('measure')))
-        self.write('SENS:{}:RANG:AUTO ON'.format(self.get_mode('measure')))
-
-    def set_limit(self, voltage = None, current = None):
-        if voltage != None:
-            self.write('SENS:VOLT:PROT {}'.format(voltage))
-            if voltage == 'MAX':
-                self.voltage_limit = 210.
-            else:
-                self.voltage_limit = voltage
-        if current != None:
-            self.write('SENS:CURR:PROT {}'.format(current))
-            if current == 'MAX':
-                self.current_limit = 1.
-            else:
-                self.current_limit = current
-
-    def set_output(self, voltage = None, current = None):
-        """
-        Only one output accepted (voltage will override current if both present).
-        This functions helps switching modes.
-        """
-        if voltage != None:
-            if self.get_mode('source') != 'VOLT':
-                self.set_mode('VOLT')
-            # self compliance
-            if voltage <= self.voltage_limit:
-                self.write('SOUR:VOLT {:f}'.format(voltage))
-            else:
-                print('Warning: Compliance Reached!!')
-                self.write('SOUR:VOLT {:f}'.format(self.voltage_limit))
-
-        elif current != None:
-            if self.get_mode('source') != 'CURR':
-                self.set_mode('CURR')
-            # self compliance
-            if current <= self.current_limit:
-                self.write('SOUR:CURR {:f}'.format(current))
-            else:
-                print('Warning: Compliance Reached!!')
-                self.write('SOUR:CURR {:f}'.format(self.current_limit))
-
-    def read_values(self):
-        return list(map(float, self.ask(':READ?').split(',')))
 
     def on(self):
-        "Turn Keithley on"
+        #"Turn Keithley on"
         self.write('OUTP ON')
     def off(self):
-        "Turn Keithley off"
+        #"Turn Keithley off"
+        self.write('OUTP OFF')
+        
+    def sweep(self, start, end, step):
+        self.write('OUTP ON')
+        self.write(":PAGE:MEAS:VAR1:START %f" % float(start))
+        self.write(":PAGE:MEAS:VAR1:STOP %f" % float(end))
+        self.write(":PAGE:MEAS:VAR1:STEP %f" % float(step))
+        self.write(":PAGE:MEAS:SSTOP COMP")
+        self.write(":PAGE:SCON:SING")
+        self.write("*WAI")
         self.write('OUTP OFF')
 
+    ### These are wrappers for common use with Keithley 2400
+    def get_mode(self, key):
+        pass
+
+    def set_mode(self, mode):
+        pass
+
+    def set_limit(self, voltage = None, current = None):
+        pass
+
+    def set_output(self, voltage = None, current = None):
+        if voltage != None:
+            self.sweep(voltage,voltage,0)
+    
+    def read_values(self):
+        self.write(":FORM:DATA ASC")
+        I_data = self.manager.query_ascii_values(":DATA? 'ID' ")[0]
+        V_data = self.manager.query_ascii_values(":DATA? 'VD' ")[0]
+        return V_data, I_data
 
 ### This is only for testing - to be removed ###
 if __name__ == '__main__':
-    import time
     # test
-    sc = SourceMeter()
-    sc.set_limit(voltage=10, current=0.12)
-    sc.on()
-
-    #sc.sweep(np.arange(0, 5, 0.1))
-
-    #while sc.busy:
-    #    time.sleep(0.5)
-    #print(sc.read_buffer()[-1])
-    print("Voltage:",sc.read_values()[0]," Current:",sc.read_values()[1])
+    an = Analyzer('GPIB0::17::INSTR')
+    an.set_output(voltage = 1)
+    #sweep = an.sweep(0,0,0.1)
+    #print(sweep[0], sweep[1])    
+    print("Voltage:",an.read_values()[0]," Current:",an.read_values()[1])
     pass
-
-
-
-
