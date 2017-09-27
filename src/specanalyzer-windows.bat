539c842c3576c3429544fe0f65e2d84185500f82
feranick <feranick@hotmail.com>
2017-09-27 13:27:29 -0400

    Updated Windows Launcher

--- src/specanalyzer-windows.bat
+++ src/specanalyzer-windows.bat
@@ -1,5 +1,5 @@
-SET PATH=%PATH%;PATH=C:\ProgramData\Anaconda3
-SET gesat-path=%~dp0
-cd %gesat-path%
-python gridedge_AT_run.py
+SET PATH=%PATH%;
+SET specanalyzer-path=%~dp0
+cd %gspecanalyzer-path%
+python SpecAnalyzer.py
 
