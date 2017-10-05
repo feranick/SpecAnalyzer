0a000265d8536c2eee2b18ed5e08140e79bfa966
Nicola Ferralis <feranick@hotmail.com>
2017-10-05 15:24:36 -0400

    Bumped to version 0.5.9

--- src/setup.py
+++ src/setup.py
@@ -5,7 +5,7 @@ setup(
     packages=find_packages(),
     install_requires=['numpy', 'matplotlib', 'pillow', 'PyQt5', 'ThorlabsPM100', 'pyvisa', 'opencv-python', 'pandas'],
     entry_points={'gui_scripts' : ['specanalyzer=SpecAnalyzer.__main__:main']},
-    version='0.5.6',
+    version='0.5.9',
     description='Measurements of Current/Voltage profiles for photovoltaic solar cells',
     long_description= """ Measurements of Current/Voltage profiles for photovoltaic solar cells """,
     author='Nicola Ferralis',
